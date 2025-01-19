d88a749b8989a283bc8e9a5e9ed2204a1a4c6da8
GAURAV KUMAR <atkola12345@gmail.com>
2025-01-19 17:23:22 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,js}', // Add paths to your template files
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      'newBlack': '#FFF',
+    },
+  },
+  plugins: [],
+}
