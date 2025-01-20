697a081c9d3a20287774e408eba976e491cb8faa
pratyush0898 <pratyush.xdev@gmail.com>
2025-01-20 20:53:57 +0530

    Add navigation bar and layout structure; include SVG assets and global styles

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ import Footer from "./components/Footer/Footer.js";
 import "./app.css";
 
 document.querySelector("#app").innerHTML = `
-
+    <header class="header"></header>
+    <main class="main"></main>
+    <footer class="footer"></footer>
 `
 
 NavBar();
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,7 +3,28 @@ import INFO from "../../data/data.js";
 
 const NavBar = () => {
   document.querySelector(".header").innerHTML = `
-
+    <nav class="top-nav">
+        <div class="left-nav">
+            <div class="nav-ghlogo">
+                <img src=${INFO.assets.ghlogo} alt="nav gh-logo" />
+            </div>
+            <div class="nav-title">
+                <span>${INFO.main.username}</span>
+            </div>
+        </div>
+        <div class="right-nav">
+            <div class="nav-logo">
+                <img src=${INFO.assets.logo} alt="nav logo" />
+            </div>
+        </div>
+    </nav>
+    <nav class="bott-nav">
+        <a href="/" class="nav-item"><img src=${INFO.assets.book} />Overview</a>
+        <a href="/skills" class="nav-item"><img src=${INFO.assets.psychology} />Skills</a>
+        <a href="/projects" class="nav-item"><img src=${INFO.assets.folder} />Projects</a>
+        <a href="/blog" class="nav-item"><img src=${INFO.assets.article} />Blog</a>
+        <a href="/contact" class="nav-item"><img src=${INFO.assets.mail} />Contact</a>
+    </nav>
   `
 }
 
