57237dfa0d8a9608ad54a96802c5d86744f8cfc8
pratyush0898 <pratyush.xdev@gmail.com>
2025-01-20 18:42:24 +0530

    Initlize Vite Vannila.js

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import NavBar from "./components/NavBar/NavBar.js";
+import Footer from "./components/Footer/Footer.js";
+import "./app.css";
+
+document.querySelector("#app").innerHTML = `
+
+`
+
+NavBar();
+Footer();
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,10 @@
+import "./NavBar.css";
+import INFO from "../../data/data.js";
+
+const NavBar = () => {
+  document.querySelector(".header").innerHTML = `
+
+  `
+}
+
+export default NavBar;
